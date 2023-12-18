b685b13f0b11e62678691a56db5638e2aa18956f
lhmuana <edd_vai@yahoo.co.in>
2023-12-18 16:31:07 +0530

    printing multiple variable inside one tag

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,16 +10,23 @@ const root = ReactDOM.createRoot(el);
 
 // create a component
 function App(){
-    let message = "bye there";
+    let message = "A condition a diklo e";
+    let m1 ='this is the first variable';
+    let m2 = 'this is the second variable';
     if(Math.random()>0.5){
-        message ="a condition a dik e";
+        message ="A condition a dik e";
         //variable ah hian string leh number chauh hman tur. A dang chu a diklo ang.
         //object phei chu a diklo lehzual ang
-    }
+        }
+
+    let date = new Date();
+    let time = date.toLocaleTimeString();
     return(
         <>
            <h1>Hi there</h1> 
            <h1>{message}</h1>
+           <h1>{time}</h1>
+           <h2>this is the multiple variable code: var1: {m1} and var2:{m2}</h2>
         </>
     );
 } 
