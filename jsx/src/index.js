9a8f96f415e737c58934e10dcf4842a2e71ea9ea
lhmuana <edd_vai@yahoo.co.in>
2023-12-18 17:10:31 +0530

    Converting JSX elements with props

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -10,23 +10,19 @@ const root = ReactDOM.createRoot(el);
 
 // create a component
 function App(){
-    let message = "A condition a diklo e";
-    let m1 ='this is the first variable';
-    let m2 = 'this is the second variable';
-    if(Math.random()>0.5){
-        message ="A condition a dik e";
-        //variable ah hian string leh number chauh hman tur. A dang chu a diklo ang.
-        //object phei chu a diklo lehzual ang
-        }
 
-    let date = new Date();
-    let time = date.toLocaleTimeString();
+    const inputType = "number";
+    const minValue = 5;
+    const maxValue = 15;
     return(
         <>
            <h1>Hi there</h1> 
-           <h1>{message}</h1>
-           <h1>{time}</h1>
-           <h2>this is the multiple variable code: var1: {m1} and var2:{m2}</h2>
+           <input type={inputType} min={minValue} max={maxValue}/>
+           <h1>Enter the age</h1>
+           <input type={"number"}
+                  min={0}
+                  list={[1,2,3,4,5]}
+                  style={{color: 'green', border: '3px solid red'}}  />
         </>
     );
 } 
