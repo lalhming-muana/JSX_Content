c691e9375d0b76181eccde8c80af002a70a9b067
lhmuana <edd_vai@yahoo.co.in>
2023-12-18 20:29:24 +0530

    Extracting components App is exported to a separate file

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,31 +1,13 @@
 // import react and reactDOM
 import React from 'react';
-import  ReactDOM  from 'react-dom/client';
+import ReactDOM  from 'react-dom/client';
+import App from './App';
 
 //div ID root kha la chhuak rawh.
 const el = document.getElementById('root');
 
 // Let React control that element
 const root = ReactDOM.createRoot(el);
-
-// create a component
-function App(){
-
-    const inputType = "number";
-    const minValue = 5;
-    const maxValue = 15;
-    return(
-        <>
-           <h1>Hi there</h1> 
-           <input type={inputType} min={minValue} max={maxValue}/>
-           <h1>Enter the age</h1>
-           <input type={"number"}
-                  min={0}
-                  list={[1,2,3,4,5]}
-                  style={{color: 'green', border: '3px solid red'}}  />
-        </>
-    );
-} 
 
 // component tihlan tur
 root.render(<App />)
